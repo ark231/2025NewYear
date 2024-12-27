84b2b14f918164bd97013f42e44ad739eba9a1cb
ark231 <arklp20909@gmail.com>
2024-12-27 15:49:28 +0900

    change: IFFからRIFFに変更
    普通にRIFFの範囲で十分だった&LEだから

--- conv_font/FixedHeightFontFormat.py
+++ conv_font/FixedHeightFontFormat.py
@@ -12,18 +12,18 @@ class FourCC(bytes):
 
 
 class Chunk:
-    __chunk_id: FourCC
+    chunk_id: FourCC
     size: int
     data: bytes
 
     def __init__(self, chunk_id=b"NULL", data=b""):
-        self.__chunk_id = chunk_id
+        self.chunk_id = chunk_id
         self.size = len(data)
         self.data = data
 
     def dumps(self):
         self.size = len(self.data)
-        return struct.pack("<4sI", self.__chunk_id, self.size) + self.data + (b"\x00" if self.size % 2 == 1 else b"")
+        return struct.pack("<4sI", self.chunk_id, self.size) + self.data + (b"\x00" if self.size % 2 == 1 else b"")
 
     def dump(self, out):
         try:
@@ -33,42 +33,22 @@ class Chunk:
                 outfile.write(self.dumps())
 
 
-class Form(Chunk):
-    chunk_id = b"FORM"
-    __form_id: FourCC
-    members: list[Chunk]
-
-    def __init__(self, form_id=b"NULL", members=None):
-        super().__init__(chunk_id=Form.chunk_id)
-        self.__form_id = form_id
-        if members is None:
-            members = []
-        self.members = members
-
-    def dumps(self):
-        self.data = self.__form_id
-        for member in self.members:
-            self.data += member.dumps()
-        return super().dumps()
-
-
 class List(Chunk):
-    item_type: FourCC
-    items: list[Chunk]
-
     chunk_id = b"LIST"
+    list_type: FourCC
+    children: list[Chunk]
 
-    def __init__(self, item_type=b"NULL", items=None):
+    def __init__(self, list_type=b"NULL", children=None):
         super().__init__(chunk_id=List.chunk_id)
-        self.item_type = item_type
-        if items is None:
-            items = []
-        self.items = items
+        self.list_type = list_type
+        if children is None:
+            children = []
+        self.children = children
 
     def dumps(self):
-        self.data = self.item_type
-        for item in self.items:
-            self.data += item.dumps()
+        self.data = self.list_type
+        for child in self.children:
+            self.data += child.dumps()
         return super().dumps()
 
 
@@ -114,21 +94,21 @@ class CMAPTable(Chunk):
         return super().dumps()
 
 
-class CMAP(Form):
-    form_id = b"CMAP"
+class CMAP(List):
+    list_type = b"CMAP"
 
     def __init__(self):
-        super().__init__(form_id=CMAP.form_id)
+        super().__init__(list_type=CMAP.list_type)
         self.table_1byte = CMAPTable(1)
         self.table_2byte = CMAPTable(2)
         self.table_3byte = CMAPTable(3)
         self.table_4byte = CMAPTable(4)
 
     def dumps(self):
-        self.members.append(self.table_1byte)
-        self.members.append(self.table_2byte)
-        self.members.append(self.table_3byte)
-        self.members.append(self.table_4byte)
+        self.children.append(self.table_1byte)
+        self.children.append(self.table_2byte)
+        self.children.append(self.table_3byte)
+        self.children.append(self.table_4byte)
         return super().dumps()
 
 
@@ -139,27 +119,42 @@ class GlyphShape(Chunk):
         super().__init__(chunk_id=GlyphShape.chunk_id)
 
 
-class Glyph(Form):
-    form_id = b"GLYF"
+class ShapeList(List):
+    list_type = b"SPLI"
 
     def __init__(self):
-        super().__init__(form_id=Glyph.form_id)
-        self.shapes = List(GlyphShape.chunk_id)
+        super().__init__(list_type=CMAP.list_type)
+
+
+class Glyph(List):
+    list_type = b"GLYF"
+
+    def __init__(self):
+        super().__init__(list_type=Glyph.list_type)
+        self.shapes = ShapeList()
 
     def dumps(self):
-        self.members.append(self.shapes)
+        self.children.append(self.shapes)
         return super().dumps()
 
 
-class FixedHeightFont(Form):
+class RIFFHeader(List):
+    chunk_id = b"RIFF"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.chunk_id = RIFFHeader.chunk_id
+
+
+class FixedHeightFont(RIFFHeader):
     def __init__(self):
-        super().__init__(form_id=b"FHFT")
+        super().__init__(list_type=b"FHFT")
         self.metadata = FontMetadata()
         self.cmap = CMAP()
         self.glyph = Glyph()
 
     def dumps(self):
-        self.members.append(self.metadata)
-        self.members.append(self.cmap)
-        self.members.append(self.glyph)
+        self.children.append(self.metadata)
+        self.children.append(self.cmap)
+        self.children.append(self.glyph)
         return super().dumps()
